be6e45d8ff46c3034dd6dd5e5896fb3266316b6b
saurabhyadav63 <saurabhyadav6386@gmail.com>
2024-02-01 17:28:54 +0530

    first commit

---
+++ front/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SAURABH</div>
+          <div className="right">
+            <p>Dehradun , UK-INDIA</p>
+            <p>Open: 10:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By SAURABH YADAV</p>
+          </div>
+       
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
